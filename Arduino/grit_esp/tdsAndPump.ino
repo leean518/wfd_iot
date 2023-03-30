6a460259ac6bf61485418b2b0e5ee289a9411442
redfire359 <112633870+redfire359@users.noreply.github.com>
2023-03-29 23:20:34 -0400

    Add files via upload

---
+++ Arduino/grit_esp/tdsAndPump.ino
@@ -0,0 +1,157 @@
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// GPIO 5 D1
+#define pin1 13
+#define testpin 2
+#define tdsPin A0
+#define VREF 3.3              // analog reference voltage(Volt) of the ADC
+#define SCOUNT  30            // sum of sample point
+
+int analogBuffer[SCOUNT];     // store the analog value in the array, read from ADC
+int analogBufferTemp[SCOUNT];
+int analogBufferIndex = 0;
+int copyIndex = 0;
+
+float averageVoltage = 0.0;
+float tdsValue = 0.0;
+float temperature = 23.0;       // current temperature for compensation
+
+// WiFi
+const char *ssid = "hydro"; // Enter your WiFi name
+const char *password = "hydrohydro";  // Enter WiFi password
+
+// MQTT Broker
+const char *mqtt_broker = "192.168.1.179";
+const char *topic = "grit/pump";
+const char *tds_topic = "grit/tds";
+const char *mqtt_username = "emqx";
+const char *mqtt_password = "public";
+const int mqtt_port = 1883;
+int *level;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+    Serial.begin(115200);  // Set software serial baud to 115200;
+
+    pinMode(pin1, OUTPUT);
+    pinMode(testpin, OUTPUT);
+    pinMode(tdsPin, INPUT);
+    
+    // connecting to a WiFi network
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.println("Connecting to WiFi..");
+    }
+    Serial.println("Connected to the WiFi network");
+    //connecting to a mqtt broker
+    client.setServer(mqtt_broker, mqtt_port);
+    client.setCallback(callback);
+    while (!client.connected()) {
+        const char *client_id = "esp8266-client-";
+        Serial.println("Connecting to public emqx mqtt broker.....");
+        if (client.connect(client_id)) {
+            Serial.println("Public emqx mqtt broker connected");
+        } else {
+            Serial.print("failed with state ");
+            Serial.print(client.state());
+            delay(2000);
+        }
+    }
+    // publish and subscribe
+    client.subscribe(topic);
+}
+
+
+void callback(char *topic, byte *payload, unsigned int length) {
+    Serial.print("Message arrived in topic: ");
+    Serial.println(topic);
+    Serial.print("Message:");
+    String message;
+    for (int i = 0; i < length; i++) {
+        message = message + (char) payload[i];  // convert *byte to string
+    }
+    Serial.print(message);
+    
+    if (strcmp(topic,"grit/pump")==0) {
+      if (message == "off") { 
+        digitalWrite(pin1, LOW);
+        digitalWrite(testpin, LOW);
+      }   // LED on
+      if (message == "on") {
+        digitalWrite(pin1, HIGH);
+        digitalWrite(testpin, HIGH); 
+      }
+    }
+
+    Serial.println();
+    Serial.println("-----------------------");
+}
+
+void loop() {
+
+  static unsigned long analogSampleTimepoint = millis();
+  
+  if(millis()-analogSampleTimepoint > 40U){     //every 40 milliseconds,read the analog value from the ADC
+    analogSampleTimepoint = millis();
+    analogBuffer[analogBufferIndex] = analogRead(tdsPin);    //read the analog value and store into the buffer
+    analogBufferIndex++;
+    if(analogBufferIndex == SCOUNT){ 
+      analogBufferIndex = 0;
+    }
+  }   
+  
+  static unsigned long printTimepoint = millis();
+  if(millis()-printTimepoint > 800U){
+    printTimepoint = millis();
+    for(copyIndex=0; copyIndex<SCOUNT; copyIndex++){
+      analogBufferTemp[copyIndex] = analogBuffer[copyIndex];
+      
+      // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+      averageVoltage = getMedianNum(analogBufferTemp,SCOUNT) * (float)VREF / 1024.0;
+      float compensationCoefficient = 1.0 + 0.02 * (temperature - 25.0);
+      float compensationVoltage = averageVoltage / compensationCoefficient;
+      tdsValue = (133.42 * compensationVoltage * compensationVoltage * compensationVoltage - 255.86 * compensationVoltage * compensationVoltage + 857.39 * compensationVoltage) * 0.5;
+
+      
+      Serial.print("TDS Value: ");
+     // Serial.print(tdsValue, 0);
+      Serial.println(analogRead(tdsPin));
+      Serial.println(" ppm");
+
+      char str[20];
+      sprintf(str, "%i",analogRead(tdsPin));
+      client.publish(tds_topic, str);
+      delay(500);
+      client.loop();      
+    }
+  }
+}
+
+int getMedianNum(int bArray[], int iFilterLen){
+  int bTab[iFilterLen];
+  for (byte i = 0; i<iFilterLen; i++)
+  bTab[i] = bArray[i];
+  int i, j, bTemp;
+  for (j = 0; j < iFilterLen - 1; j++) {
+    for (i = 0; i < iFilterLen - j - 1; i++) {
+      if (bTab[i] > bTab[i + 1]) {
+        bTemp = bTab[i];
+        bTab[i] = bTab[i + 1];
+        bTab[i + 1] = bTemp;
+      }
+    }
+  }
+  if ((iFilterLen & 1) > 0){
+    bTemp = bTab[(iFilterLen - 1) / 2];
+  }
+  else {
+    bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+  }
+  return bTemp;
+}
+
